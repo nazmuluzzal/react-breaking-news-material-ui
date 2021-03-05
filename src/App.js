55c1c6a096b5a1f55d2c692ea3540958d0989e9c
nazmuluzzal <uzzalnazmul@gmail.com>
2021-03-06 01:45:10 +0600

    added AXIOS JS for load data

--- src/App.js
+++ src/App.js
@@ -3,27 +3,40 @@ import Button from "@material-ui/core/Button";
 
 import "./App.css";
 import News from "./components/News/News";
+import axios from "axios";
 
 function App() {
   const [articles, setArticles] = useState([]);
+
+  // useEffect(() => {
+  //   const url =
+  //     "https://newsapi.org/v2/top-headlines?country=us&apiKey=a9c2c1fde411408380cc3c5811cd7134";
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => setArticles(data.articles));
+  // }, []);
+
+  // Data Loading by axios JS
+  //
   useEffect(() => {
     const url =
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=a9c2c1fde411408380cc3c5811cd7134";
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setArticles(data.articles));
+    axios(url).then((data) => setArticles(data.data.articles));
   }, []);
+
   return (
-    <div>
+    <div className="main-container">
       <h1>Material UI</h1>
       <Button variant="contained" color="primary">
         Hello World
       </Button>
       {/* <News></News> */}
       <h2>Top News: {articles.length}</h2>
-      {articles.map((article) => (
-        <News article={article}></News>
-      ))}
+      <div className="news-container">
+        {articles.map((article) => (
+          <News article={article}></News>
+        ))}
+      </div>
     </div>
   );
 }
